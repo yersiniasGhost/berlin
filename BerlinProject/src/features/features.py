Fixed data streamer sample tools normalization

BerlinProject/src/features/features.py
@@ -1,10 +1,11 @@
 import numpy as np
 import talib
 
+
 #
 def calculate_sma_tick(period: int, data: np.array, history: int = 0) -> float:
-    sma = talib.SMA(data[-(period+history):], period)
-    return sma[-(history+1):]
+    sma = talib.SMA(data[-(period + history):], period)
+    return sma[-(history + 1):]
 
 
 # def calculate_macd_tick(data: np.array, fast_period: int = 12, slow_period: int = 26, signal_period: int = 9,
@@ -24,8 +25,8 @@ def calculate_macd_tick(data: np.array, fast_period: int = 12, slow_period: int 
     window_data = data[-window_size:]
 
     if window_size > len(window_data):
-        raise ValueError(f"Insufficient data for expected window size {window_size} and data length: {len(window_data)}")
-
+        raise ValueError(
+            f"Insufficient data for expected window size {window_size} and data length: {len(window_data)}")
 
     macd, signal, hist = talib.MACD(window_data, fastperiod=fast_period, slowperiod=slow_period,
                                     signalperiod=signal_period)
@@ -34,7 +35,6 @@ def calculate_macd_tick(data: np.array, fast_period: int = 12, slow_period: int 
     signal_values = signal[-(history + 1):]
     hist_values = hist[-(history + 1):]
     return macd_values, signal_values, hist_values
-
 
 # def calculate_macd_tick(data: np.array, fast_period: int = 12, slow_period: int = 26, signal_period: int = 9,
 #                         history: int = 0) -> tuple:
@@ -50,8 +50,6 @@ def calculate_macd_tick(data: np.array, fast_period: int = 12, slow_period: int 
 #         signal_values = signal[-(history + 1):]
 #         hist_values = hist[-(history + 1):]
 #         return macd_values, signal_values, hist_values
-
-
 
 
 # data = np.arange(1, 31, dtype=float)